df65b16dac3787725a76a259f2559be2a43f4786
xrotwang <xrotwang@googlemail.com>
2016-07-25 09:09:14 +0200

    added textcrh

--- pyglottolog/monsterlib/_bibtex_escaping.py
+++ pyglottolog/monsterlib/_bibtex_escaping.py
@@ -1,34 +1,19 @@
 # coding: utf-8
-# _bibtex_escaping.py - latex escapes and unicode bracket escapes
+"""
+Unfortunately out bibtex source files are not encoded consistently across files. I.e.
+latex commands with the same name may be used with different definitions in different
+files.
 
-"""TODO: replace in source files OR add de-/encoding rule
-
-\textsubdot{
-\textsubbar{
-\textsuperscript{ c
-\~e
-\textsubline{
-\texthtd
-\textvertline
-\textesh
-\textbaru
-\textbari
-\textsubring
-\textgamma
-\textupsilon
+So far identified problems:
+- \:n is used in sala.bib for N WITH DOT ABOVE, but is also defined in the textipa
+  package, commands of which have been used in various other source files.
 
 
-\texthooktop{
-\textsubwedge{
-\textemdash
-\textendash
-\textthreequarters
-\texthooktopd
-\textrevglotstop
-\;
-\:
-
----, --, ``'', {}, \textdot, \textsubdot,  \v{j} -> \v\j
+See also:
+- textipa package reference
+  http://www.ling.ohio-state.edu/~mbeckman/ling600.01/tipachart.pdf
+- comprehensive latex symbol list
+  http://www.math.boun.edu.tr/instructors/gurel/symbols-a4.pdf
 
 see also:
   _bibtex_undiacritic.py rules
@@ -36,10 +21,6 @@ see also:
   http://github.com/clld/clld/blob/master/clld/lib/latex.py
   http://github.com/mcmtroffaes/latexcodec/blob/develop/latexcodec/codec.py#L97
 
-FIXME: tex-escape/character entity mix in anla.bib titlealt
-
-\textpolhook{ -> \k{}?
-\&
 """
 
 import re
@@ -53,7 +34,6 @@ __all__ = [
     'u_escape', 'u_unescape', 'latex_to_utf8', 'ulatex_postprocess', 'ulatex_preprocess']
 
 LATEX_TABLE = {
-    u'COMBINING DOT ABOVE': [br'\.', br'\textdot'],
     u'COMBINING DOT BELOW': br'\textsubdot',
     u'COMBINING MACRON': [br'\=', br'\textbar'],
     u'COMBINING MACRON BELOW': br'\textsubbar',
@@ -64,6 +44,7 @@ LATEX_TABLE = {
     u'COMBINING CIRCUMFLEX ACCENT': br'\^',
     u'COMBINING ACUTE ACCENT': br"\'",
     u'COMBINING GRAVE ACCENT': br"\`",
+    u'COMBINING DIAERESIS': br'\"',
     u'COMBINING LATIN SMALL LETTER C': br'\textsuperscript{c}',
     u'COMBINING RING BELOW': br'\textsubring',
     u'COMBINING OGONEK': br'\textpolhook',
@@ -72,18 +53,34 @@ LATEX_TABLE = {
     u'COMBINING BREVE BELOW': br'\textsubu',
     u'COMBINING BRIDGE BELOW': br'\textsubbridge',
 
+    # FIXME: may clash with commands from the textipa package!!
+    u'COMBINING HORN': br'\;',
+    u'COMBINING DOT ABOVE': [br'\.', br'\textdot', br'\:'],
+    # FIXME: that's what textipa provides: but not how it is used in sala.bib!
+    #u'LATIN SMALL LETTER N WITH RETROFLEX HOOK': [br'\:{n}', br'\:n'],
+
+    u'MASCULINE ORDINAL INDICATOR': br'\textordmasculine',
+    u'LEFT SINGLE QUOTATION MARK': [br'\textquoteleft', br'\grq'],
+    u'SINGLE LOW-9 QUOTATION MARK': br'\glq',
+    u'RIGHT SINGLE QUOTATION MARK': br'\textquoteright',
+    u'NOT SIGN': br'\textlnot',
+    u'VULGAR FRACTION THREE QUARTERS': br'\textthreequarters',
+    u'LATIN SMALL LETTER A WITH RING ABOVE': br'\r{a}',
+    u'LATIN CAPITAL LETTER A WITH RING ABOVE': br'\r{A}',
     u'MODIFIER LETTER SMALL W': br'\textsuperscript{w}',
     u'SUPERSCRIPT ONE': br'\textsuperscript{1}',
     u'SUPERSCRIPT TWO': br'\textsuperscript{2}',
     u'SUPERSCRIPT THREE': br'\textsuperscript{3}',
     u'SUPERSCRIPT FOUR': br'\textsuperscript{4}',
+    u'SUPERSCRIPT LATIN SMALL LETTER N': br'\textsuperscript{n}',
     u'PERCENT SIGN': br'\%',
-    u'LATIN SMALL LETTER D WITH HOOK': [br'\texthtd', br'\texthooktopd'],
+    u'LATIN SMALL LETTER D WITH HOOK': [br'\texthtd', br'\texthooktopd', br'\textrhooktopd'],
+    u'LATIN SMALL LETTER D WITH TAIL': br'\:{d}',
     u'LATIN SMALL LETTER B WITH HOOK': [br'\texthtb', br'\texthooktopb'],
     u'LATIN SMALL LETTER N WITH LEFT HOOK': br'\textltailn',
     u'LATIN SMALL LETTER S WITH HOOK': br'\textrtails',
     u'LATIN SMALL LETTER ETH': br'\dh',
-    u'LATIN SMALL LETTER ENG': br'\ng',
+    u'LATIN SMALL LETTER ENG': [br'\ng', br'\textipa{N}'],
     u'LATIN CAPITAL LETTER ENG': br'\NG',
     u'LATIN SMALL LETTER OPEN O': br'\textopeno',
     u'LATIN SMALL LETTER THORN': br'\textthorn',
@@ -107,13 +104,16 @@ LATEX_TABLE = {
     u'PLUS-MINUS SIGN': br'\plusminus',
     u'LATIN SMALL LETTER E WITH MACRON AND ACUTE': br'\textacutemacron e',
     u'LATIN SMALL LETTER E WITH OGONEK': br'\textpolhook e',
+    u'LATIN SMALL LETTER A WITH OGONEK': br'\c{a}',
+    u'LATIN SMALL LETTER TURNED M': br'\textturnm',
+    u'MODIFIER LETTER TRIANGULAR COLON': br'\textlengthmark',
     u'LESS-THAN SIGN': br'\textless',
     u'GREATER-THAN SIGN': br'\textgreater',
     # a bit of a misuse, but that's how it is used in goba.bib:
     u'LATIN CROSS': br'\textbarpipe',
     # again a mis-use:
     u'LATIN SMALL LETTER OPEN E': br'\textepsilon',
-    u'LATIN SMALL LETTER GAMMA': br'\textgamma',
+    u'LATIN SMALL LETTER GAMMA': [br'\textgamma', br'\gamma'],
     u'LATIN SMALL LETTER TURNED V': br'\textturnv',
     u'GREEK SMALL LETTER BETA': br'\textbeta',
     u'GREEK CAPITAL LETTER ETA': br'\textEta',
@@ -125,6 +125,8 @@ LATEX_TABLE = {
     u'DEGREE SIGN': br'\circ',
     u'VERTICAL LINE': br'\textvertline',
     u'DOUBLE VERTICAL LINE': br'\textdoublevertline',
+    u'LATIN SMALL LETTER T WITH RETROFLEX HOOK': br'\:{t}',
+    u'LATIN SMALL LETTER H WITH STROKE': br'\textcrh',
 }
 
 _LC_TABLES = (
@@ -133,7 +135,8 @@ _LC_TABLES = (
 assert u'\xe4'.encode('latex') == r'\"a'
 assert r'\"a'.decode('latex') == u'\xe4'
 
-for unicode_text, latex_text in LATEX_TABLE.iteritems():
+for unicode_text, latex_text in sorted(
+        list(LATEX_TABLE.items()), key=lambda t: t[0], reverse=True):
     if not isinstance(latex_text, (list, tuple)):
         latex_text = [latex_text]
     uchar = unicodedata.lookup(unicode_text)
@@ -142,6 +145,96 @@ for unicode_text, latex_text in LATEX_TABLE.iteritems():
             table.register(uchar, lt)
 
 
+numcharref_patterns = [
+    re.compile('\\\\?&#(?P<dec>[0-9]+);'),
+    re.compile('\?\[\\\\u\s*(?P<dec>[0-9]+)\]')]
+
+command_patterns = [
+    re.compile('\\\\%s \{(?P<arg>[^\}]+)\}' % name)
+    for name in ['url', 'emph', 'textit', 'texttt']]
+
+
+def numcharref_repl(m):
+    return unichr(int(m.group('dec')))
+
+
+def is_combining(c):
+    try:
+        return unicodedata.name(c).startswith('COMBINING')
+    except ValueError:
+        return False
+
+
+def ulatex_preprocess(s):
+    s = unicodedata.normalize('NFC', s.decode('utf8'))
+    for c in s:
+        if is_combining(c):
+            print s
+            raise ValueError
+    return s
+
+
+language_tags = {
+    'latin': 'lat',
+    'zh': 'zho',
+    'hindi': 'hin',
+    'eng': 'eng',
+    'viet': 'vie',
+    'tib': 'bod',
+    'skt': 'san',  # Sanskrit
+    'gujarati': 'guj',
+    'pacoh': 'pac',
+    'thai': 'tha',
+    'dutch': 'nld',
+    'burm': 'mya',
+    'dan': 'dan',
+    'norw': 'nor',
+    'oldkhmer': 'qok',
+    'ital': 'ita',
+    'santali': 'sat',
+    'span': 'spa',
+    'germ': 'deu',
+    'fr': 'fra',
+    'rus': 'rus'}
+language_tag_pattern = re.compile(
+    u"\\\\(?P<tag>%s)\s+" % '|'.join(list(language_tags.keys())))
+
+
+def recode_language_tags(s):
+    def repl(m):
+        return '[%s] ' % language_tags[m.group('tag')]
+    return language_tag_pattern.sub(repl, s)
+
+
+def ulatex_postprocess(s, debracket=re.compile(u"\{([^\}]+)\}")):
+    if not isinstance(s, text_type):
+        s = s.decode('ascii')
+    for p in command_patterns:
+        s = p.sub(lambda m: m.group('arg'), s)
+    s = debracket.sub(u"\\1", debracket.sub(u"\\1", s.replace('{}', '')))
+    n = []
+    comb = []
+    for c in s:
+        if is_combining(c):
+            comb.append(c)
+        else:
+            n.append(c)
+            while comb:
+                n.append(comb.pop())
+    s = u''.join(n)
+    for p in numcharref_patterns:
+        s = p.sub(numcharref_repl, s)
+    for cmd in ['relax', 'it', 'em', 'textsc', 'cite', 'citet']:
+        s = s.replace('\\' + cmd + ' ', '')
+    s = recode_language_tags(s)
+    return unicodedata.normalize('NFC', s)
+
+
+def ulatex_decode(s):
+    return ulatex_postprocess(ulatex_preprocess(s).decode('ulatex'))
+
+
+# legacy
 def u_escape(s):
     def iterchunks(s):
         for c in s:
@@ -192,59 +285,6 @@ def _find_u_escape(encoding):
 
 
 codecs.register(_find_u_escape)
-
-numcharref_patterns = [
-    re.compile('\\\\?&#(?P<dec>[0-9]+);'),
-    re.compile('\?\[\\\\u\s*(?P<dec>[0-9]+)\]')]
-
-command_patterns = [
-    re.compile('\\\\%s\{(?P<arg>[^\}]+)\}' % name) for name in ['url', 'emph']]
-
-
-def numcharref_repl(m):
-    return unichr(int(m.group('dec')))
-
-
-def is_combining(c):
-    try:
-        return unicodedata.name(c).startswith('COMBINING')
-    except ValueError:
-        return False
-
-
-def ulatex_preprocess(s):
-    s = s.decode('utf8')
-    for p in numcharref_patterns:
-        s = p.sub(numcharref_repl, s)
-    for p in command_patterns:
-        s = p.sub(lambda m: m.group('arg'), s)
-    s = unicodedata.normalize('NFC', s)
-    for c in s:
-        if is_combining(c):
-            print s
-            raise ValueError
-    return s
-
-
-def ulatex_postprocess(s, debracket=re.compile(u"\{([^\}]+)\}")):
-    if not isinstance(s, text_type):
-        s = s.decode('ascii')
-    s = debracket.sub(u"\\1", debracket.sub(u"\\1", s.replace('{}', '')))
-    n = []
-    comb = []
-    for c in s:
-        if is_combining(c):
-            comb.append(c)
-        else:
-            n.append(c)
-            while comb:
-                n.append(comb.pop())
-    return unicodedata.normalize('NFC', u''.join(n))
-
-
-def ulatex_decode(s):
-    return ulatex_postprocess(ulatex_preprocess(s).decode('ulatex'))
-
 
 # legacy
 
