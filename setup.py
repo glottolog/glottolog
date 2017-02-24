a1747b9c8541ecad7dd8740e2a1c25dc0b24c494
xrotwang <xrotwang@googlemail.com>
2017-02-24 08:33:48 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -21,16 +21,18 @@ requires = [
 
 setup(
     name='pyglottolog',
-    version='0.2',
+    version='0.3',
     description='python package for glottolog data curation',
     long_description='',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
     ],
-    author='',
-    author_email='',
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
     url='',
     keywords='data linguistics',
     packages=find_packages(),
