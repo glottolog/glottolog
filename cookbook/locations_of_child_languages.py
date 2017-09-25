e70ada45043e68cbee852b8c125ad223f8b6b217
Robert Forkel <xrotwang@googlemail.com>
2017-09-25 17:27:11 +0200

    Create locations_of_child_languages.py

---
+++ cookbook/locations_of_child_languages.py
@@ -0,0 +1,18 @@
+from clldutils.dsv import UnicodeWriter
+from pyglottolog.api import Glottolog
+from pyglottolog.objects import Level
+
+
+def locations(glottolog, fid, outpath):
+    with UnicodeWriter(outpath) as writer:
+        writer.writerow(['name', 'glottocode', 'latitude', 'longitude'])
+        for lang in glottolog.languoids():
+            if lang.level == Level.language and lang.latitude is not None:
+                if fid in [l[1] for l in lang.lineage]:
+                    writer.writerow([lang.name, lang.id, lang.latitude, lang.longitude])
+
+
+if __name__ == '__main__':
+    import sys
+
+    locations(Glottolog(sys.argv[1]), sys.argv[2], sys.argv[3])
