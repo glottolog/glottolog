bfd765271b68c1102a9ae86a83f608d556d1091b
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-12-04 13:41:25 +0100

    run current tests under pytest and travis

--- setup.py
+++ setup.py
@@ -1,45 +1,48 @@
 from setuptools import setup, find_packages
 
-
-requires = [
-    # list required third-party packages here
-    'six>=1.9',
-    'clldutils>=1.9.1',
-    'pybtex>=0.20',
-    'latexcodec',
-    'unidecode',
-    'whoosh',
-    'attrs',
-    'pycountry>=17.01.08',
-    'termcolor',
-    'newick',
-    'markdown',
-    'bs4',
-    'requests',
-]
-
 setup(
     name='pyglottolog',
     version='2.0.0.dev0',
-    description='python package for glottolog data curation',
-    long_description='',
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-    ],
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
-    url='https://github.com/clld/glottolog',
+    description='python package for glottolog data curation',
     keywords='data linguistics',
+    license='',
+    url='https://github.com/clld/glottolog',
     packages=find_packages(),
+    platforms='any',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     include_package_data=True,
     zip_safe=False,
-    install_requires=requires,
     entry_points={
         'console_scripts': ['glottolog=pyglottolog.__main__:main'],
     },
-    tests_require=['mock'],
-    test_suite="pyglottolog")
+    install_requires=[
+        'six>=1.9',
+        'clldutils>=1.9.1',
+        'pybtex>=0.20',
+        'latexcodec',
+        'unidecode',
+        'whoosh',
+        'attrs',
+        'pycountry>=17.01.08',
+        'termcolor',
+        'newick',
+        'markdown',
+        'bs4',
+        'requests',
+    ],
+    extras_require={
+        'test': ['mock>=2', 'pytest>=3.3', 'pytest-mock', 'pytest-cov'],
+    },
+    long_description='',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
