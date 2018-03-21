84a0a9f11e58ed042eb861b3273a8264b8543b35
xrotwang <xrotwang@googlemail.com>
2018-03-21 15:45:36 +0100

    enhanced script: allow passing in repos location

--- cookbook/treemaker/glottolog_tree.py
+++ cookbook/treemaker/glottolog_tree.py
@@ -5,10 +5,10 @@ from treemaker import TreeMaker
 from newick import loads
 
 
-def make_tree(*taxa):
+def make_tree(repos, *taxa):
     # We create a dict to lookup Glottolog languoids by name, ISO- or Glottocode.
     langs = {}
-    for lang in Glottolog().languoids():
+    for lang in Glottolog(repos).languoids():
         if lang.iso:
             langs[lang.iso] = lang
         langs[lang.name] = lang
@@ -28,9 +28,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--format', help='output format [nexus|newick|ascii]', default='ascii')
+    parser.add_argument('--repos', default=None)
     parser.add_argument('taxa', nargs=argparse.REMAINDER)
     args = parser.parse_args()
-    tree = make_tree(*args.taxa)
+    tree = make_tree(args.repos, *args.taxa)
     if args.format == 'ascii':
         print(loads(tree.write())[0].ascii_art())
     else:
